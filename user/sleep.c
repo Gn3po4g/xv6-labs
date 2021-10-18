11ed05c1de79e51856a376e7acfc91c64059f5b0
Noroby <gnepoag@outlook.com>
2021-10-18 11:28:45 +0800

    xv6

--- user/sleep.c
+++ user/sleep.c
@@ -2,6 +2,16 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("missing argument!\n");
+    exit(1);
+  } else if (argc > 2) {
+    printf("too many arguments!\n");
+    exit(1);
+  } else {
+    sleep(atoi(argv[1]));
+    printf("(nothing happens for a little while)\n");
+  }
+  exit(0);
 }
