cfd63f3f2b88f4d0322c637a40bd77614d9914a2
Noroby <gnepoag@outlook.com>
2021-10-20 11:58:19 +0800

    my solution for util lab exercise 1

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,39 +4,18 @@
 
 int main(int argc, char *argv[]) {
   int parentpipe[2], childpipe[2];
-  char buf[1];
+  char buf[5];
   pipe(parentpipe);
   pipe(childpipe);
   if (fork() == 0) {
-    close(parentpipe[0]);
-    close(childpipe[1]);
-    if (read(childpipe[0], buf, sizeof(buf)) != sizeof(buf)) {
-      printf("child read error");
-      exit(1);
-    }
-    close(childpipe[0]);
-    printf("%d: received ping\n", getpid());
-    if (write(parentpipe[1], buf, sizeof(buf)) != sizeof(buf)) {
-      printf("child write error");
-      exit(1);
-    }
-    close(parentpipe[1]);
-    exit(0);
+    read(parentpipe[0], buf, 4);
+    printf("%d: received %s\n", getpid(), buf);
+    write(childpipe[1], "pong", strlen("pong"));
   } else {
-    close(parentpipe[1]);
-    close(childpipe[0]);
-    if (write(childpipe[1], buf, sizeof(buf)) != sizeof(buf)) {
-      printf("parent write error");
-      exit(1);
-    }
-    close(childpipe[1]);
-    if (read(parentpipe[0], buf, sizeof(buf)) != sizeof(buf)) {
-      printf("parent read error");
-      exit(1);
-    }
-    close(parentpipe[0]);
-    printf("%d: received pong\n", getpid());
+    write(parentpipe[1], "ping", strlen("ping"));
     wait(0);
-    exit(0);
+    read(childpipe[0], buf, 4);
+    printf("%d: received %s\n", getpid(), buf);
   }
+  exit(0);
 }
--- user/primes.c
+++ user/primes.c
@@ -2,4 +2,44 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {}
+void mapping(int n, int pd[]) {
+  close(n);
+  dup(pd[n]);
+  close(pd[0]);
+  close(pd[1]);
+}
+
+void primes() {
+  int previous, next;
+  int fd[2];
+  if (read(0, &previous, sizeof(int))) {
+    printf("prime %d\n", previous);
+    pipe(fd);
+    if (fork() == 0) {
+      mapping(1, fd);
+      while (read(0, &next, sizeof(int)))
+        if (next % previous != 0)
+          write(1, &next, sizeof(int));
+    } else {
+      wait(0);
+      mapping(0, fd);
+      primes();
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int fd[2];
+  pipe(fd);
+  if (fork() == 0) {
+    mapping(1, fd);
+    for (int i = 2; i < 36; i++) {
+      write(1, &i, sizeof(int));
+    }
+  } else {
+    wait(0);
+    mapping(0, fd);
+    primes();
+  }
+  exit(0);
+}
